40c8fb757effdb05414eba45bc0810ac6ae90e4d
Nikoloz Dolidze <n.dolidze@noxtton.com>
2026-05-15 16:58:30 +0400

    Remove ability to delete only auth code (#511)

--- src/containers/RecordDetails/RecordDetailsV2.tsx
+++ src/containers/RecordDetails/RecordDetailsV2.tsx
@@ -19,7 +19,7 @@ import {
   TrashOutlined
 } from '@tetherto/pearpass-lib-ui-kit/icons'
 // @ts-expect-error - declaration file is incomplete
-import { RECORD_TYPES, useRecordById } from '@tetherto/pearpass-lib-vault'
+import { useRecordById } from '@tetherto/pearpass-lib-vault'
 
 import { RecordItemIcon } from '../../components/RecordItemIcon'
 import { useRouter } from '../../context/RouterContext'
